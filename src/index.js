ae8614eed5f4b1ac8cf5abe9059bc88aa715fb70
Michael Westbom <michael@agilion.com>
2016-03-03 09:52:12 -0500

    Working react build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Yay")
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import 'index.html'
+
+ReactDOM.render(
+  <div>Oh hai</div>,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -23,6 +24,19 @@ module.exports = {
         loader: 'babel',
         include: path.resolve(__dirname, 'src'),
       },
+    ],
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./src'),
+      'node_modules',
     ],
   },
   devServer: {
