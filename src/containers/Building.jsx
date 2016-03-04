60113922025c5e86e56a3da5a55b5d2d47274001
Michael Westbom <michael@agilion.com>
2016-03-04 10:56:41 -0500

    Teach how to buy buildings

--- src/containers/Building.jsx
+++ src/containers/Building.jsx
@@ -1,6 +1,16 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { buyBuilding } from 'ducks/machines'
 
-export default ({ building }) => (
-  <div>{ building.get('cost') }</div>
+const Building = ({ dispatch, building }) => (
+  <div>
+    { building.get('count') } | { building.get('cost') }
+    <button onClick={buy(dispatch, building)}>Buy</button>
+  </div>
 )
+
+const buy = (dispatch, building) => () => {
+  dispatch(buyBuilding(building))
+}
+
+export default connect()(Building)
